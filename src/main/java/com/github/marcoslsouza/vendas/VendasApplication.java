9bfcbb37f21ce547191548ec3b16cb7d01c3f3eb
Marcos L. Souza <marcoslsouzadf@gmail.com>
2020-12-12 17:54:18 -0300

    Criacao das entidades Cliente e Servicos

--- src/main/java/com/github/marcoslsouza/vendas/VendasApplication.java
+++ src/main/java/com/github/marcoslsouza/vendas/VendasApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.marcoslsouza.vendas.model.entity.Cliente;
+
 @SpringBootApplication
 @RestController
 public class VendasApplication {
---
+++ src/main/java/com/github/marcoslsouza/vendas/model/entity/Cliente.java
@@ -0,0 +1,32 @@
+package com.github.marcoslsouza.vendas.model.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+// O Lombok cria automaticamente em tempo de execução os gets e seters.
+
+@Entity
+@Data
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column(name = "data_cadastro")
+	private LocalDate dataCadastro;
+	
+}
