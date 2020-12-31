3360d30128c70f3afdd7f1312cbefb2da335fdda
Marcos L. Souza <marcoslsouzadf@gmail.com>
2020-12-31 18:42:41 -0300

    Ajustes em UsuarioService e messages.properties

--- src/main/java/com/github/marcoslsouza/vendas/rest/UsuarioController.java
+++ src/main/java/com/github/marcoslsouza/vendas/rest/UsuarioController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,12 +29,14 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED) // 201
 	public void salvar(@RequestBody @Valid Usuario usuario) {
 		try {
+			usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 			usuarioService.salvar(usuario);
 		} catch (UsuarioCadastradoException e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
